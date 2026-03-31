91dfde1102618cd031a137b1b5f5fc9c0ac1e6ef
Sujoy Das <me@binarycodes.io>
2026-04-01 00:20:09 +0300

    use location for weather, use environment vars where possible

--- dotfiles/dot-config/scripts/weather.sh
+++ dotfiles/dot-config/scripts/weather.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-weather=$(curl -s "wttr.in/?format=%c%t" 2>/dev/null)
+weather=$(curl -s "wttr.in/$LOCATION?format=%c%t" 2>/dev/null)
 
 if [ -z "$weather" ] || echo "$weather" | grep -q "Unknown"; then
     echo '{"text": "N/A", "tooltip": "Weather unavailable"}'
     exit 0
 fi
 
-tooltip=$(curl -s "wttr.in/?format=%l:+%C+%t+%h+%w" 2>/dev/null)
+tooltip=$(curl -s "wttr.in/$LOCATION?format=$LOCATION:+%C+%t+%h+%w" 2>/dev/null)
 
 printf '{"text": "%s", "tooltip": "%s"}\n' "$weather" "$tooltip"
