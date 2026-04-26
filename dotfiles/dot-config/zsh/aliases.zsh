156ba08d10469450c1956c25fa0401989d668455
Sujoy Das <me@binarycodes.io>
2026-04-26 20:39:06 +0300

    zsh aliases / functions

--- dotfiles/dot-config/zsh/aliases.zsh
+++ dotfiles/dot-config/zsh/aliases.zsh
@@ -1,2 +1,3 @@
 alias task="/usr/bin/go-task"
 alias arch="/usr/bin/go-task --taskfile $XDG_CONFIG_HOME/arch/taskfile.yml"
+alias gitlog='git log --abbrev-commit --pretty=format:"%C(magenta)%h %C(red)% G? %Cgreen%ad (%>(15,trunc)%cr)  %Cblue%<(15,trunc)%an %Creset%s %Cred% gD% D" --date="format-local:%Y-%m-%d %H:%M:%S"'
